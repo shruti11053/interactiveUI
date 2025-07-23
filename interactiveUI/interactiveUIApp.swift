b3641b744582b6291ff9993a2da916ef442a6f49
Shruti Shivakumar <shivakumarshruti1@gmail.com>
2025-07-23 16:40:42 -0400

    Initial Commit

---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by Shruti Shivakumar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ interactiveUITests/interactiveUITests.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUITests.swift
+//  interactiveUITests
+//
+//  Created by Shruti Shivakumar on 7/23/25.
+//
+
+import Testing
+@testable import interactiveUI
+
+struct interactiveUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
